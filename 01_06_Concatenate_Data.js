f9c5fec5a510e4afee83c5da64c81b260235dce6
Pavlin Vasilev <shadow_psv@yahoo.com>
2023-09-20 17:05:49 +0300

    Done!

---
+++ 01_06_Concatenate_Data.js
@@ -0,0 +1,10 @@
+function concatenateData(input) {
+    let firstName = input[0];
+    let lastName = input[1];
+    let age = Number(input[2]);
+    let city = input[3];
+    
+    let result = `You are ${firstName} ${lastName}, a ${age}- years old person from  ${city}.`;
+    console.log(result);
+}
+concatenateData(["Maria", "Ivanova", "20", "Sofia"]);
