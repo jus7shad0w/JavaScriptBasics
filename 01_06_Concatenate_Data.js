322c6c62f05cc21a4709628846acd32ae7f38c3f
Pavlin Vasilev <shadow_psv@yahoo.com>
2023-09-20 17:17:07 +0300

    Sixt exercise complete.

--- 01_06_Concatenate_Data.js
+++ 01_06_Concatenate_Data.js
@@ -3,7 +3,6 @@ function concatenateData(input) {
     let lastName = input[1];
     let age = Number(input[2]);
     let city = input[3];
-    
     let result = `You are ${firstName} ${lastName}, a ${age}- years old person from  ${city}.`;
     console.log(result);
 }
