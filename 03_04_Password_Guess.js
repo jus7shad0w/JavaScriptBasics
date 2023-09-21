40781136e60da995b50fc6ae939d85f251722661
Pavlin Vasilev <shadow_psv@yahoo.com>
2023-09-21 21:43:24 +0300

    Lesson three exercise two complete.

---
+++ 03_04_Password_Guess.js
@@ -0,0 +1,11 @@
+function guessPassword(input) {
+    let password = input[0];
+    let correctPassword = "s3cr3t!P@ssw0rd";
+
+    if (password === correctPassword) {
+        console.log("Welcome");
+    } else {
+        console.log("Wrong Password")
+    }
+}
+guessPassword((["qwerty"]));
