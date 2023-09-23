b6a63a9eae07d0b7f3537bc67efcefab4b2011e6
Pavlin Vasilev <shadow_psv@yahoo.com>
2023-09-23 16:59:52 +0300

    Personal Titles complete!

--- 5.Conditional Statements Advanced-Lab/04_PersonalTitles.js
+++ 5.Conditional Statements Advanced-Lab/04_PersonalTitles.js
@@ -1,8 +1,25 @@
 function personalTitles(input) {
     let age = Number(input[0]);
     let gender = input[1];
-    if (gender == f){
-        if
+
+    if (gender == "f") {
+        if (age >= 16) {
+            console.log("Ms.");
+        } else if (age < 16) {
+            console.log("Miss");
+        } else {
+            console.log("Incorect age");
+        }
+    } else if (gender == "m") {
+        if (age >= 16) {
+            console.log("Mr.");
+        } else if (age < 16) {
+            console.log("Master");
+        } else {
+            console.log("Incorect age");
+        }
+    } else {
+        console.log("Wrong gender");
     }
 }
-personalTitles(["12","f"])
+personalTitles(["12", "f"])
