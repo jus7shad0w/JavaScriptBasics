9f4107453de7f8b38ec2a25e4c348318930118fe
Pavlin Vasilev <shadow_psv@yahoo.com>
2023-09-23 18:13:54 +0300

    09_FruitOrVegetable ver 1 and 2

---
+++ 5.Conditional Statements Advanced-Lab/09_FruitOrVegetable_ver2.js
@@ -0,0 +1,22 @@
+function fruitOrVegetable(input) {
+    let productInput = input[0];
+    switch (productInput) {
+        case "banana":
+        case "apple":
+        case "kiwi":
+        case "cherry":
+        case "lemon":
+        case "grapes":
+            console.log("fruit");
+            break;
+        case "tomato":
+        case "cucumber":
+        case "pepper":
+        case "carrot":
+            console.log("vegetable");
+            break;
+        default:
+            console.log("unknown");
+    }
+}
+fruitOrVegetable(["banana"])
