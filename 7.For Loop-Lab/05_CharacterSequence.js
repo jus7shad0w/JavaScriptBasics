5a06aa51b36c0d533cd4595bc2a4371eea6873a0
Pavlin Vasilev <shadow_psv@yahoo.com>
2023-12-13 20:27:01 +0200

    05_CharacterSequence.js

---
+++ 7.For Loop-Lab/05_CharacterSequence.js
@@ -0,0 +1,8 @@
+function printColon(input) {
+    let word = input[0];
+    for (let i = 0; i < word.length; i++) {
+        console.log(word[i]);
+    }
+}
+printColon(["softuni"])
+printColon(["ice cream"])
