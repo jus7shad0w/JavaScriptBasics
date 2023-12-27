99e21eac9e714829510e11ee002ac2ed69492c39
Pavlin Vasilev <shadow_psv@yahoo.com>
2023-12-27 17:59:50 +0200

    02_Password.js

---
+++ 9.While Loop-Lab/02_Password.js
@@ -0,0 +1,13 @@
+function password(input) {
+    let userName = input[0];
+    let password = input[1];
+    let data = input[2];
+    let i = 3;
+    while (data !== password) {
+        data = input[i];
+        i++;
+    }
+    console.log(`Welcome ${userName}!`);
+}
+password(["Nakov", "1234", "Pass", "1324", "1234"])
+password(["Gosho", "secret", "secret"])
